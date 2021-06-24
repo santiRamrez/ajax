2af69f441a0c077848c656cadc6742b84b35f8a3
Santiago Ramirez <santi_coding@outlook.com>
2021-06-23 21:45:12 -0400

    I did send data from my form to a php file using POST method

--- send_email.php
+++ send_email.php
@@ -1,58 +1,13 @@
 <?php
-  // Message Vars
-	$msg = '';
-	$msgClass = '';
+  echo 'Procesing...';
 
-	// Check For Submit
-	if(filter_has_var(INPUT_POST, 'submit')){
-		// Get Form Data
-    $name = htmlspecialchars($_POST['name']);
-    $phone = htmlspecialchars($_POST['phone']);
-    $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
-		$message = htmlspecialchars($_POST['message']);
+	//Check for GET variable
+	if(isset($_GET['name'])) {
+		echo 'GET: Your name is ' . $_GET['name'];
+	}
 
-		// Check Required Fields
-		if(!empty($email) && !empty($name) && !empty($message) && !empty($phone) && !empty($subject)){
-			// Passed
-			// Check Email
-			if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-				// Failed
-				$msg = 'Por favor use un email válido';
-				$msgClass = 'alert-danger';
-			} else {
-				// Passed
-				$toEmail = 'ymejias@mejiasasociados.cl';
-				$subject1 = 'Cliente Sitio Web '.$name;
-				$body = '<h2>Datos Cliente</h2>
-          <h4>Nombre</h4><p>'.$name.'</p>
-          <h4>Teléfono</h4><p>'.$phone.'</p>
-          <h4>Email</h4><p>'.$email.'</p>
-          <h4>Asunto</h4><p>'.$subject.'</p>
-					<h4>Message</h4><p>'.$message.'</p>
-				';
-
-				// Email Headers
-				$headers = "MIME-Version: 1.0" ."\r\n";
-				$headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
-
-				// Additional Headers
-				$headers .= "From: " .$name. "<".$email.">". "\r\n";
-
-				if(mail($toEmail, $subject1, $body, $headers)){
-					// Email Sent
-					$msg = 'Tu mensaje ha sido enviado correctamente';
-					$msgClass = 'alert-success';
-				} else {
-					// Failed
-					$msg = 'Error, tu mensaje no fue enviado';
-					$msgClass = 'alert-danger';
-				}
-			}
-		} else {
-			// Failed
-			$msg = 'Por favor llene todos los campos';
-			$msgClass = 'alert-danger';
-		}
+	//Check for POST variable
+	if(isset($_POST['data'])) {
+		echo 'POST: Your name is ' . $_POST['data'];
 	}
 ?>
